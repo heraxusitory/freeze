843bcc97bcc8e224eeddbc0f4c1c77b49ac7e7ad
Davut <dsergeeb@smartdatatech.io>
2021-02-03 22:11:58 +0300

    Added header styles

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -18,7 +18,28 @@
 
     <!-- Styles -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-{{--    <link href="{{ asset('css/app.css') }}" rel="stylesheet">--}}
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/tools.css') }}" rel="stylesheet">
     @yield('css')
 </head>
 <body>
+<header>
+    <div class="container">
+        <div class="media-display">
+        </div>
+        <div class="text-display">
+            <div>
+                <h1 class="main_title">{{ env('APP_NAME') }}</h1>
+                <div class="subtitle">
+                    <p>Доверься мастеру, а сам отдохни</p>
+                </div>
+            </div>
+        </div>
+        <ul class="nav custom-main_navigation">
+            <li class="nav-item"><a class="nav-link" href="#">Главная</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">Оставить заявку</a></li>
+            <li class="nav-item"><a class="nav-link" href="#">Отзывы</a></li>
+        </ul>
+    </div>
+</header>
+<div id="app">
