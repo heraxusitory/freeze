d7824e2bbe8ac1cfa1e5a75be9520d983edc51b5
Davut <dsergeeb@smartdatatech.io>
2021-02-08 23:01:47 +0300

    Fix scripts including, new form for questions

---
+++ resources/views/layouts/forms/question.blade.php
@@ -0,0 +1,7 @@
+<form action="#" method="POST">
+    @csrf
+    <div class="form-group">
+        <label for="question" class="title">Спросите, что вас интересует</label>
+        <textarea id="question" class="for-control"></textarea>
+    </div>
+</form>
