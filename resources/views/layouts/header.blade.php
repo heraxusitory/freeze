b54631cf3a642393861f67ac3c906e5bd0fb3031
Davut <dsergeeb@smartdatatech.io>
2021-02-09 23:05:37 +0300

    Fix navigation

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -40,7 +40,6 @@
             <li class="nav-item"><a class="nav-link application" href="#" data-url="{{route('request.form.get')}}" data-toggle="modal" data-target="#modal-application">Оставить заявку</a></li>
             <li class="nav-item"><a class="nav-link" href="#">Отзывы</a></li>
             <li class="nav-item"><a class="nav-link application" href="#" data-url="{{route('question.form.get')}}" data-toggle="modal" data-target="#modal-application">Задать вопрос</a></li>
-            <li class="nav-item"><a class="nav-link" href="#"></a></li>
         </ul>
     </div>
 </header>
