5811513aebc6ca3068021608a76ec646380f83bc
Davut <dsergeeb@smartdatatech.io>
2021-02-14 18:35:49 +0300

    Fix

--- resources/views/main/reviews.blade.php
+++ resources/views/main/reviews.blade.php
@@ -7,8 +7,7 @@
             <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
 
                 <div class="review" style="word-break: break-word">
-                    <h5>{{$review->name}} <span>{{$review->email}}</span></h5>
-{{--                    <span>{{$review->rating}}</span>--}}
+                    <h5>{{$review->name}} <span>{{($review->email) ? "({$review->email})" : "" }}</span></h5>
                     <div class="star-rating" style="display: flex; justify-content: left">
                         @for($count = 1; $count <= $review->rating; $count++)
                             <img src="{{ asset('/img/icons/star-rating.svg') }}">
@@ -19,27 +18,6 @@
 
             </div>
         @endforeach
-        {{--        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">--}}
-        {{--            <div class="review">--}}
-        {{--                <h5>Name <span>(почтовый адрес скрыт)</span></h5>--}}
-        {{--                <span>rating</span>--}}
-        {{--                <p>Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum</p>--}}
-        {{--            </div>--}}
-        {{--        </div>--}}
-        {{--        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">--}}
-        {{--            <div class="review">--}}
-        {{--                <h5>Name <span></span></h5>--}}
-        {{--                <span>rating</span>--}}
-        {{--                <p>Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum</p>--}}
-        {{--            </div>--}}
-        {{--        </div>--}}
-        {{--        <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">--}}
-        {{--            <div class="review">--}}
-        {{--                <h5>Name <span>(example@email.com)</span></h5>--}}
-        {{--                <span>rating</span>--}}
-        {{--                <p>Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum</p>--}}
-        {{--            </div>--}}
-        {{--        </div>--}}
     </div>
     @endif
 </div>
