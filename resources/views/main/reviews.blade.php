3745720f8aa0b53bffe29cda64e6e40791f47249
Davut <dsergeeb@smartdatatech.io>
2021-02-14 18:37:33 +0300

    Fix

--- resources/views/main/reviews.blade.php
+++ resources/views/main/reviews.blade.php
@@ -1,23 +1,23 @@
 <div class="container">
     <div class="row">
         @if(!$isExistRecords)
-               <div> Здесь может появиться Ваш отзыв!</div>
+            <div> Здесь может появиться Ваш отзыв!</div>
         @else
-        @foreach($reviews as $review)
-            <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+            @foreach($reviews as $review)
+                <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
 
-                <div class="review" style="word-break: break-word">
-                    <h5>{{$review->name}} <span>{{($review->email) ? "({$review->email})" : "" }}</span></h5>
-                    <div class="star-rating" style="display: flex; justify-content: left">
-                        @for($count = 1; $count <= $review->rating; $count++)
-                            <img src="{{ asset('/img/icons/star-rating.svg') }}">
-                        @endfor
+                    <div class="review" style="word-break: break-word">
+                        <h5>{{$review->name}} <span>{{($review->email) ? "({$review->email})" : "" }}</span></h5>
+                        <div class="star-rating" style="display: flex; justify-content: left">
+                            @for($count = 1; $count <= $review->rating; $count++)
+                                <img src="{{ asset('/img/icons/star-rating.svg') }}">
+                            @endfor
+                        </div>
+                        <p>{{$review->review}}</p>
                     </div>
-                    <p>{{$review->review}}</p>
-                </div>
 
-            </div>
-        @endforeach
+                </div>
+            @endforeach
+        @endif
     </div>
-    @endif
 </div>
